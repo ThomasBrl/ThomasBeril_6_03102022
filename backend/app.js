df1f23db9aaa27010c79071b0d7dd23bada257ac
Thomas <thms.beril@gmail.com>
2023-01-16 11:48:58 +0100

    remove gitignore files

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,20 @@
-const express = require('express');
-
 const app = express();
+const express = require('express');
+const mongoose = require('mongoose');
+mongoose.connect('mongodb+srv://clusterTest:<clusterTest>@cluster0-pme76.mongodb.net/test?retryWrites=true&w=majority',
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
+
 app.use(express.json());
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  next();
+});
 
 app.use('/api/stuff', (req, res, next) => {
   const stuff = [
@@ -25,12 +38,6 @@ app.use('/api/stuff', (req, res, next) => {
   res.status(200).json(stuff);
 });
 
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-  next();
-});
 
 app.post('/api/stuff', (req, res, next) => {
   console.log(req.body);
